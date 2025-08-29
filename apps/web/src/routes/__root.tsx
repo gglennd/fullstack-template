7f0ebc8b8b919ae2c151e4921c627e5ad4d03507
gglennd <grdimaano18@gmail.com>
2025-08-29 14:06:52 +0800

    Implement tanstack/react-router

---
+++ apps/web/src/routes/__root.tsx
@@ -0,0 +1,16 @@
+/// <reference types="vite/client" />
+import { createRootRoute, Outlet } from "@tanstack/react-router";
+import * as React from "react";
+
+export const Route = createRootRoute({
+  component: RootComponent,
+});
+
+function RootComponent() {
+  return (
+    <React.Fragment>
+      <div>Hello "__root"!</div>
+      <Outlet />
+    </React.Fragment>
+  );
+}
--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,6 +1,15 @@
+import { tanstackRouter } from "@tanstack/router-plugin/vite";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    tsconfigPaths(),
+    tanstackRouter({
+      target: "react",
+      autoCodeSplitting: true,
+    }),
+    react(),
+  ],
 });
